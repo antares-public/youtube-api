52c3bf4ea8dd72b3f1b073f0fd2821fcb6198f73
mariaverestovaya <rainbowdeity69@gmail.com>
2021-05-08 18:18:29 +0500

    design

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,23 @@
 import React from 'react';
-import { Button } from 'antd';
+import { Input, Button } from 'antd';
+import styled from "styled-components"
 
 export const App: React.FC = () => {
-  return <Button type="primary">Primary Button</Button>
+  return <Center><Form>
+    <Input placeholder="Basic usage" />
+    <Input.Password className="my-1" placeholder="input password" />
+    <Button type="primary">Send</Button>
+  </Form></Center>
 }
+
+const Center = styled.div`
+  display: grid;
+  place-items: center;
+  height: 100vh;
+`;
+
+const Form = styled.div`
+  text-align: center;
+  max-width: 300px;
+`;
+
